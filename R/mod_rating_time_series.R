4a3c08aec8a8bf7790f413f707b96da9ba3746d5
Mark McPherson <mark.mcpherson1@nhs.net>
2022-06-21 10:10:33 +0100

    Add module for timeseries chart

--- R/mod_rating_time_series.R
+++ R/mod_rating_time_series.R
@@ -10,7 +10,7 @@
 mod_rating_time_series_ui <- function(id){
   ns <- NS(id)
   tagList(
-    highchartOuput("rating_time_series")
+    highchartOutput(ns("rating_time_series"))
   )
 }
 
@@ -19,64 +19,60 @@ mod_rating_time_series_ui <- function(id){
 #' @noRd
 mod_rating_time_series_server <- function(r, id){
   moduleServer(id, function(input, output, session){
-
-
-
     output$rating_time_series <- renderHighchart({
-
-      chart <- data %>%
+      chart <- r$filtered_data %>%
         select(date, rating) %>%
         mutate(
           week_num = (year(date) - year(min(date))) * 52 + week(date) - week(min(date)),
-          date = nextweekday(min(date) %m+% weeks(week_num), 6)
+          date = nextweekday(min(date) %m+% weeks(week_num), "Friday")
         ) %>%
         group_by(date, rating) %>%
         summarise(n = n(), .groups = "drop_last") %>%
+        complete(crossing(rating = 1:5), fill = list(n = 0)) %>%
         ungroup() %>%
-        mutate(per = n / sum(n)) %>%
+        mutate(per = round(100 * n / sum(n), 1)) %>%
         suppressWarnings() %>%
-        complete(nesting(date, per), fill = list(per = 0)) %>%
-        pivot_wider(names_from = rating, values_from = per) %>%
-        setNames(c("date", "sunny", "sunny_with_clouds", "cloudy", "rainy", "stormy"))
+        pivot_wider(names_from = rating, values_from = c(per, n)) %>%
+        mutate(base = rowSums(select(., starts_with("n"))))
 
         cols <- viridis(5)
         cols <- substr(cols, 0, 7)
 
         highchart() %>%
           hc_add_series(
+            chart,
             type = "line",
-            name = "sunny",
-            color = cols[1],
-            data = chart,
-            hcaes(x = date, y = sunny)
+            hcaes(x = date, y = per_1, b = n_1),
+            name = "Sunny",
+            color = cols[1]
           ) %>%
           hc_add_series(
+            chart,
+            hcaes(x = date, y = per_2, b = n_2),
             type = "line",
-            name = "sunny",
-            color = cols[1],
-            data = chart,
-            hcaes(x = date, y = sunny)
+            name = "Partially Sunny",
+            color = cols[2]
           ) %>%
           hc_add_series(
+            chart,
+            hcaes(x = date, y = per_3, b = n_3),
             type = "line",
-            name = "sunny",
-            color = cols[1],
-            data = chart,
-            hcaes(x = date, y = sunny)
+            name = "Cloudy",
+            color = cols[3]
           ) %>%
           hc_add_series(
+            chart,
+            hcaes(x = date, y = per_4, b = n_4),
             type = "line",
-            name = "sunny",
-            color = cols[1],
-            data = chart,
-            hcaes(x = date, y = sunny)
+            name = "Rainy",
+            color = cols[4]
           ) %>%
           hc_add_series(
+            chart,
             type = "line",
-            name = "sunny",
-            color = cols[1],
-            data = chart,
-            hcaes(x = date, y = sunny)
+            hcaes(x = date, y = per_5, b = n_5),
+            name = "Stormy",
+            color = cols[5]
           ) %>%
           hc_xAxis(
             type = "datetime",
@@ -86,24 +82,24 @@ mod_rating_time_series_server <- function(r, id){
           ) %>%
           hc_yAxis(min = 0) %>%
           hc_tooltip(
-            formatter = JS(
-              "function () {
-          // The first returned item is the header, subsequent items are the
-          // points
-          var options = {year: 'numeric', month: 'short'};
-          var dtf = new Intl.DateTimeFormat('en-EN', options);
+            formatter = JS("
+              function () {
+                // The first returned item is the header, subsequent items are the
+                // points
+                var options = {year: 'numeric', month: 'short', day: 'numeric'};
+                var dtf = new Intl.DateTimeFormat('en-EN', options);
 
-          return ['<b>' + dtf.format(this.x) + '</b>'].concat(
-            this.points ?
-              this.points.map(function (point) {
-              console.log(point)
-                b = point.point.b ? ' (base: ' + point.point.b + ')' : '';
-                c = point.point.c ? '<br>' + point.point.c : ''
-                return '<span style=\"color: ' + point.series.color + '\"><strong>' + point.series.name + ': ' + point.y + ' ' + b + '</strong></span>' + c;
-              }) : []
-          );
-        }"
-            ),
+                return ['<b>' + dtf.format(this.x) + '</b>'].concat(
+                  this.points ?
+                    this.points.map(function (point) {
+                    console.log(point)
+                      b = point.point.b ? ' (base: ' + point.point.b + ')' : '';
+                      c = point.point.c ? '<br>' + point.point.c : ''
+                      return '<span style=\"color: ' + point.series.color + '\"><strong>' + point.series.name + ': ' + point.y + ' ' + b + '</strong></span>' + c;
+                    }) : []
+                );
+              }
+            "),
             split = TRUE,
             useHTML = TRUE
           ) %>%
@@ -127,7 +123,6 @@ mod_rating_time_series_server <- function(r, id){
               )
             )
           )
-
       })
   })
 }
--- R/tempcheck.R
+++ R/tempcheck.R
@@ -7,20 +7,21 @@
 #'
 #' @importFrom dplyr arrange between bind_rows case_when cur_column count desc
 #' @importFrom dplyr filter group_by left_join mutate n pull rename rename_with
-#' @importFrom dplyr rowwise select slice summarise ungroup
+#' @importFrom dplyr rowwise select slice summarise transmute ungroup
 #' @importFrom DT dataTableOutput renderDataTable datatable
 #' @importFrom glue glue
 #' @importFrom highcharter hc_add_series hc_annotations hc_exporting hc_legend
 #' @importFrom highcharter hc_plotOptions hc_tooltip hc_xAxis hc_yAxis hcaes
-#' @importFrom highcharter highchart highchartOutput renderHighchart
+#' @importFrom highcharter highchart highchartOutput renderHighchart JS
 #' @importFrom httr GET add_headers content
-#' @importFrom lubridate dmy now wday
+#' @importFrom lubridate %m+% dmy now wday week weeks year
 #' @importFrom purrr map
 #' @importFrom rlang .data := sym
 #' @importFrom shiny NS br column div fluidPage fluidRow h1 h2 HTML img
 #' @importFrom shiny moduleServer reactive renderUI selectInput
 #' @importFrom shiny shinyApp tagAppendAttributes tagList tags uiOutput
-#' @importFrom tidyr complete
+#' @importFrom tidyr complete crossing nesting pivot_wider starts_with
+#' @importFrom viridis viridis
 NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
